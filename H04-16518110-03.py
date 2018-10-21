769f829a9c4f05f889a5928be275d0cfe3d8c41c
gintkenny <gintkenny@gmail.com>
2018-10-22 00:49:42 +0700

    Update H04-16518110-03.py

--- H04-16518110-03.py
+++ H04-16518110-03.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 # NIM/Nama  : 16518110/Kendrik Emkel Ginting
 # Tanggal   : 21 Oktober 2018
 # Deskripsi : PR Data Analisis 3 H04
@@ -12,64 +6,39 @@
 
 # Kamus
 # df = dataframe database
-# dmakanan = data penjualan hanya makanan
+# dproduksi = data pegawai dproduksi
+# gu = koefisien korelasi gaji dan usia (untuk perhitungan menentukan paling berkorelasi dengan yang mana)
+# gg = koefisien korelasi gaji dan golongan (untuk perhitungan menentukan paling berkorelasi dengan yang mana)
+# gt = koefisien korelasi gaji dan tahun masuk (untuk perhitungan menentukan paling berkorelasi dengan yang mana)
 
 # Algoritma
 import pandas as pd
 
-
-# In[2]:
-
-
 df = pd.read_csv("stei-c-3.csv")
 print(df)
-
-
-# In[3]:
-
 
 # 1. Banyaknya data
 print(len(df))
 # 7651
-
-
-# In[4]:
-
 
 # 2. Data karyawan bernama Tuan Yon
 print(df.loc[df["nama"] == "Tuan Yon"])
 #           nama departemen  tahun_masuk  usia  golongan     gaji
 # 1204  Tuan Yon  teknologi         2017    31        10  9939125
 
-
-# In[7]:
-
-
 # 3. Banyaknya karyawan dengan usia kurang dari atau sama dengan 30
 print(len(df.loc[df["usia"] <= 30]))
 # 1287
 
-
-# In[17]:
-
-
 # 4. Banyaknya karyawan personalia dengan gaji di luar rentang 4 - 5 juta
 print(len(df.loc[(df["gaji"] > 5000000) | (df["gaji"] < 4000000)]))
 # 6806
-
-
-# In[20]:
-
 
 # 5. Data karyawan produksi dengan gaji maksimum
 dproduksi = df.loc[df["departemen"] == "produksi"]
 print(dproduksi[dproduksi["gaji"] == dproduksi.max()["gaji"]])
 #              nama departemen  tahun_masuk  usia  golongan     gaji
 # 3454  Nur Lawiyah   produksi         2015    31         1  9997348
-
-
-# In[23]:
-
 
 # 6. Data 10 karyawan teknologi dengan gaji terbanyak. Jika ada gaji yang sama banyak, urutkan dari usia yang termuda
 print(((df.loc[df["departemen"] == "teknologi"]).sort_values(["gaji", "usia"], ascending=[0,1]))[:10])
@@ -85,10 +54,6 @@ print(((df.loc[df["departemen"] == "teknologi"]).sort_values(["gaji", "usia"], a
 # 3323      Kharisrama Trihatmoko  teknologi         2015    27         1   9906720
 # 1955  Muhammad Isyraqi El Hakim  teknologi         2014    51         1   9905403
 
-
-# In[25]:
-
-
 # 7. Tabel frekuensi banyaknya karyawan tiap golongan
 print(df["golongan"].value_counts())
 # 7     808
@@ -102,10 +67,6 @@ print(df["golongan"].value_counts())
 # 5     738
 # 1     716
 
-
-# In[27]:
-
-
 # 8. Tabel frekuensi banyaknya karyawan tiap departemen yang masuk sebelum tahun 2015
 print((df.loc[df["tahun_masuk"] < 2015])["departemen"].value_counts())
 # produksi       469
@@ -116,25 +77,13 @@ print((df.loc[df["tahun_masuk"] < 2015])["departemen"].value_counts())
 # kreatif        424
 # keuangan       416
 
-
-# In[28]:
-
-
 # 9. Rata-rata gaji seluruh karyawan
 print(df["gaji"].mean())
 # 5516954.16638348
 
-
-# In[30]:
-
-
 # 10. Standar deviasi usia karyawan operasional
 print((df.loc[df["departemen"] == "operasional"])["usia"].std())
 # 10.398036110328745
-
-
-# In[39]:
-
 
 # 11. Dengan apakah gaji berkorelasi? Usia, tahun masuk, atau golongan? Tuliskan koefisien korelasinya
 print("Koefisien korelasi gaji dan usia adalah", df["gaji"].corr(df["usia"]))
@@ -175,4 +124,3 @@ else: # gt = gu/ koef kor. gaji tahun masuk = koef kor. gaji usia
 # Koefisien korelasi gaji dan usia adalah 0.0045757914258750245
 # Koefisien korelasi nilai fisika dan matematika adalah 0.0130926769691646
 # Koefisien korelasi nilai fisika dan matematika adalah -0.8777694422927667
-
